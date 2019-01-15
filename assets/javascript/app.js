42c0246226f6f22ed65d8f8c9560aa22aa41f605
kanikas01 <larvakurt@yahoo.com>
2019-01-14 18:19:02 -0800

    Define trivia question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,54 @@
 
 // Gameplay can begin when DOM is loaded
 $(document).ready(function () {
+
+  var question_1 = {
+    prompt: "What political party did Luanne support in the 2004 election?",
+    possibleAnswers: ["Democratic", "Republican", "Libertarian", "Communist"],
+    correctAnswer: possibleAnswers[3]
+  }
+
+  var question_2 = {
+    prompt: "What is Peggy Hill's maiden name?",
+    possibleAnswers: ["Platter", "Gribble", "Pewterschmidt", "Bouvier"],
+    correctAnswer: possibleAnswers[0]
+  }
+
+  var question_3 = {
+    prompt: "What alias does Dale Gribble use most frequently?",
+    possibleAnswers: ["Joe Jack", "Dusty Hill", "Rusty Shackelford", "Gilbert Dauterive"],
+    correctAnswer: possibleAnswers[2]
+  }
+
+  var question_4 = {
+    prompt: "What does Dale Gribble call the van he uses for his extermination business?",
+    possibleAnswers: ["The Queen Machine", "The Bugabago", "My Cadillac Car", "Lenore"],
+    correctAnswer: possibleAnswers[1]
+  }
+
+  var question_5 = {
+    prompt: "What does Hank Hill sell for a living?",
+    possibleAnswers: ["Jeans", "Tractors", "Pickup Trucks", "Propane and propane accessories"],
+    correctAnswer: possibleAnswers[3]
+  }
+
+  var question_6 = {
+    prompt: "Who is Joseph Gribble's biological father?",
+    possibleAnswers: ["John Redcorn", "Bill Dautrive", "Principal Moss", "Hank Hill"],
+    correctAnswer: possibleAnswers[0]
+  }
+
+  var question_7 = {
+    prompt: "What is Boomhauer's first name?",
+    possibleAnswers: ["Charles", "Jeffery", "Buck", "Octavio"],
+    correctAnswer: possibleAnswers[1]
+  }
+
+  var question_8 = {
+    prompt: 'Who was the voice for Elroy "Lucky" Kleinschmidt?',
+    possibleAnswers: ["Mike Judge", "Stephen Root", "Trace Adkins", "Tom Petty"],
+    correctAnswer: possibleAnswers[3]
+  }
+
   
 });
