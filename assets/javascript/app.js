ea47c51ae34ec6af295a20f7ebaa04598aba80fd
kanikas01 <larvakurt@yahoo.com>
2019-01-15 09:47:45 -0800

    Remove old question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   // Define game object
   var game = {
 
+    // Define question objects
     questions = [{
       prompt: "What political party did Luanne support in the 2004 election?",
       possibleAnswers: ["The Democratic Party", "The Republican Party", "The Libertarian Party", "The Communist Party"],
@@ -43,57 +44,6 @@ $(document).ready(function () {
     }]
   };
 
-  // Define question objects
-  var question_1 = {
-    prompt: "What political party did Luanne support in the 2004 election?",
-    possibleAnswers: ["Democratic", "Republican", "Libertarian", "Communist"],
-    correctAnswer: this.possibleAnswers[3]
-  }
-
-  var question_2 = {
-    prompt: "What is Peggy Hill's maiden name?",
-    possibleAnswers: ["Platter", "Gribble", "Pewterschmidt", "Bouvier"],
-    correctAnswer: this.possibleAnswers[0]
-  }
-
-  var question_3 = {
-    prompt: "What alias does Dale Gribble use most frequently?",
-    possibleAnswers: ["Joe Jack", "Dusty Hill", "Rusty Shackelford", "Gilbert Dauterive"],
-    correctAnswer: this.possibleAnswers[2]
-  }
-
-  var question_4 = {
-    prompt: "What does Dale Gribble call the van he uses for his extermination business?",
-    possibleAnswers: ["The Queen Machine", "The Bugabago", "My Cadillac Car", "Lenore"],
-    correctAnswer: this.possibleAnswers[1]
-  }
-
-  var question_5 = {
-    prompt: "What does Hank Hill sell for a living?",
-    possibleAnswers: ["Jeans", "Tractors", "Pickup Trucks", "Propane and propane accessories"],
-    correctAnswer: this.possibleAnswers[3]
-  }
-
-  var question_6 = {
-    prompt: "Who is Joseph Gribble's biological father?",
-    possibleAnswers: ["John Redcorn", "Bill Dautrive", "Principal Moss", "Hank Hill"],
-    correctAnswer: this.possibleAnswers[0]
-  }
-
-  var question_7 = {
-    prompt: "What is Boomhauer's first name?",
-    possibleAnswers: ["Charles", "Jeffery", "Buck", "Octavio"],
-    correctAnswer: this.possibleAnswers[1]
-  }
-
-  var question_8 = {
-    prompt: 'Who was the voice for Elroy "Lucky" Kleinschmidt?',
-    possibleAnswers: ["Mike Judge", "Stephen Root", "Trace Adkins", "Tom Petty"],
-    correctAnswer: this.possibleAnswers[3]
-  }
-
-  // Add questions to array
-  // TODO
   
   // ---------- Event listeners ---------- //
 
